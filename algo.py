0eb95ea5f9a000e7db23d0dd895f2ea7da786d7c
wendy liu <40872440+wliu11@users.noreply.github.com>
2020-08-20 23:56:06 -0700

    Initial upload

---
+++ algo.py
@@ -0,0 +1,97 @@
+import numpy as np
+from policy import Policy
+
+class ValueFunctionWithApproximation(object):
+    def __call__(self,s) -> float:
+        """
+        return the value of given state; \hat{v}(s)
+
+        input:
+            state
+        output:
+            value of the given state
+        """
+        raise NotImplementedError()
+
+    def update(self,alpha,G,s_tau):
+        """
+        Implement the update rule;
+        w <- w + \alpha[G- \hat{v}(s_tau;w)] \nabla\hat{v}(s_tau;w)
+        does the += 
+        input:
+            alpha: learning rate
+            G: TD-target
+            s_tau: target state for updating (yet, update will affect the other states)
+        ouptut:
+            None
+        """
+        raise NotImplementedError()
+
+def semi_gradient_n_step_td(
+    env, #open-ai environment
+    gamma:float,
+    pi:Policy,
+    n:int,
+    alpha:float,
+    V:ValueFunctionWithApproximation,
+    num_episode:int,
+):
+    """
+    implement n-step semi gradient TD for estimating v
+
+    input:
+        env: target environment
+        gamma: discounting factor
+        pi: target evaluation policy
+        n: n-step
+        alpha: learning rate
+        V: value function
+        num_episode: #episodes to iterate
+    output:
+        None
+    """
+
+    for episode in range(num_episode):
+        
+        state = env.reset()
+        T = np.inf
+        t = 0
+        
+        rewards = [0]
+        states = [state]
+        action = pi.action(state)
+        
+        while True:
+            if t < T:
+                next_state, reward, done, info = env.step(action)
+                states.append(next_state)
+                rewards.append(reward)
+                
+                if done:
+                    T = t + 1
+                
+                else:
+                    next_action = pi.action(next_state) 
+
+            tau = t - n + 1
+            
+            if tau >= 0:
+                G = 0
+                for i in range(tau + 1, min(tau + n, T) + 1):
+                    G += np.power(gamma, i - tau - 1) * rewards[i]
+                    
+                if tau + n < T:
+                    state = states[tau + n]
+                    G += np.power(gamma, n) * V(state)
+
+                state = states[tau]
+                V.update(alpha, G, state)
+
+            t += 1
+
+            action = next_action
+
+            if tau == T - 1:
+                break
+
+    
---
+++ nn.py
@@ -0,0 +1,44 @@
+import numpy as np
+from algo import ValueFunctionWithApproximation
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class ValueFunctionWithNN(ValueFunctionWithApproximation):
+    def __init__(self,
+                 state_dims):
+        """
+        state_dims: the number of dimensions of state space
+        """
+        super(ValueFunctionWithNN, self).__init__()
+        
+        self.network = torch.nn.Sequential(
+            torch.nn.Linear(state_dims, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 16),
+            torch.nn.ReLU(),
+            torch.nn.Linear(16, 8),
+            torch.nn.ReLU(),
+            torch.nn.Linear(8, 1),
+        )
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3, betas=(0.9,0.999))
+        self.loss_fn = torch.nn.MSELoss()
+        
+
+    def __call__(self,s):
+        self.network.eval()
+        x = self.network(torch.tensor(s, dtype=torch.float32))
+        return x.detach().numpy()[0]
+
+
+    def update(self,alpha,G,s_tau):
+        self.network.train()
+        self.optimizer.zero_grad()
+        x = torch.tensor(s_tau, dtype=torch.float32)
+        pred = self.network(x)
+        loss = self.loss_fn(pred, torch.tensor(G, dtype=torch.float32))
+        loss.backward()
+        self.optimizer.step()
+    
+
