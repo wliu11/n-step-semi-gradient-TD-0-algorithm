44c5d3ed169953c39579aa6873a4a6c396c2e7e0
wendy liu <40872440+wliu11@users.noreply.github.com>
2020-08-23 21:32:27 -0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -4,7 +4,9 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-
+"""
+Value function approximation using a linear neural network with three hidden layers
+"""
 class ValueFunctionWithNN(ValueFunctionWithApproximation):
     def __init__(self,
                  state_dims):
@@ -22,16 +24,18 @@ class ValueFunctionWithNN(ValueFunctionWithApproximation):
             torch.nn.ReLU(),
             torch.nn.Linear(8, 1),
         )
+        
+        # Initialize the adam optimizer and mean squared error loss function 
         self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3, betas=(0.9,0.999))
         self.loss_fn = torch.nn.MSELoss()
         
-
     def __call__(self,s):
         self.network.eval()
         x = self.network(torch.tensor(s, dtype=torch.float32))
         return x.detach().numpy()[0]
 
-
+    
+    # Train the neural network once, obtain loss, then take one step backward
     def update(self,alpha,G,s_tau):
         self.network.train()
         self.optimizer.zero_grad()
